86f85495340964e55c108944d1690f3b73e2e626
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-01-16 13:03:37 +0800

    rewrite

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 // const port = process.env.PORT || 3000;
 const port = 3000;
+const PROJECT_DOMAIN = process.env.PROJECT_DOMAIN;
 var exec = require("child_process").exec;
 const os = require("os");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+var sh = require('shelljs');
+var wspath = sh.exec('echo $PROJECT_INVITE_TOKEN | sha1sum | head -c 6', { silent: true }).stdout;
 var request = require("request");
 var fs = require("fs");
 var path = require("path");
@@ -28,15 +31,17 @@ app.get("/status", (req, res) => {
 //启动web
 app.get("/start", (req, res) => {
   let cmdStr =
-    "chmod +x ./web.js && ./web.js -c ./config.json >/dev/null 2>&1 &";
+    "bash start.sh";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.send("命令行执行错误：" + err);
     } else {
+      console.log("Share Link: " + stdout);
       res.send("命令行执行结果：" + "启动成功!");
     }
   });
 });
+
 
 //获取系统版本、内存信息
 app.get("/info", (req, res) => {
@@ -47,11 +52,11 @@ app.get("/info", (req, res) => {
     } else {
       res.send(
         "命令行执行结果：\n" +
-          "Linux System:" +
-          stdout +
-          "\nRAM:" +
-          os.totalmem() / 1000 / 1000 +
-          "MB"
+        "Linux System:" +
+        stdout +
+        "\nRAM:" +
+        os.totalmem() / 1000 / 1000 +
+        "MB"
       );
     }
   });
@@ -65,56 +70,32 @@ app.get("/test", (req, res) => {
   });
 });
 
-//下载web可执行文件
-app.get("/download", (req, res) => {
-  download_web((err) => {
-    if (err) res.send("下载文件失败");
-    else res.send("下载文件成功");
-  });
-});
-
 app.use(
-  "/api",
+  '/' + wspath + '*',
   createProxyMiddleware({
     target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
     changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
-    ws: true, // 是否代理websockets
-    pathRewrite: {
-      // 请求中去除/api
-      "^/api": "/qwe",
-    },
-    onProxyReq: function onProxyReq(proxyReq, req, res) {},
+    ws: true,
+    logLevel: 'error',
+    onProxyReq: function onProxyReq(proxyReq, req, res) { },
   })
 );
 
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
-  let glitch_app_url = "https://fuschia-aquatic-glider.glitch.me";
+  let glitch_app_url = `https://${PROJECT_DOMAIN}.glitch.me`;
   exec("curl " + glitch_app_url, function (err, stdout, stderr) {
-    if (err) {
-      console.log("保活-请求主页-命令行执行错误：" + err);
-    } else {
-      console.log("保活-请求主页-命令行执行成功，响应报文:" + stdout);
-    }
   });
 
   // 2.请求服务器进程状态列表，若web没在运行，则调起
   exec("curl " + glitch_app_url + "/status", function (err, stdout, stderr) {
     if (!err) {
-      if (stdout.indexOf("./web.js -c ./config.json") != -1) {
-        console.log("web正在运行");
+      if (stdout.indexOf("./web.js -c /tmp/config.json") != -1) {
       } else {
         //web未运行，命令行调起
         exec(
-          "chmod +x ./web.js && ./web.js -c ./config.json >/dev/null 2>&1 &",
-          function (err, stdout, stderr) {
-            if (err) {
-              console.log("保活-调起web-命令行执行错误：" + err);
-            } else {
-              console.log("保活-调起web-命令行执行成功!");
-            }
-          }
+          "/bin/bash start.sh"
         );
       }
     } else console.log("保活-请求服务器进程表-命令行执行错误: " + err);
@@ -125,16 +106,15 @@ setInterval(keepalive, 9 * 1000);
 
 // 初始化，下载web
 function download_web(callback) {
-  let fileName = "web.js";
-  let url =
-    "https://cdn.glitch.me/53b1a4c6-ff7f-4b62-99b4-444ceaa6c0cd/web?v=1673588495643";
-  let stream = fs.createWriteStream(path.join("./", fileName));
-  request(url)
-    .pipe(stream)
-    .on("close", function (err) {
-      if (err) callback("下载文件失败");
-      else callback(null);
-    });
+  let cmdStr =
+    "/bin/bash start.sh";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      console.log("命令行执行错误：" + err);
+    } else {
+      console.log("Share Link：" + stdout);
+    }
+  });
 }
 download_web((err) => {
   if (err) console.log("初始化-下载web文件失败");
@@ -142,3 +122,4 @@ download_web((err) => {
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
